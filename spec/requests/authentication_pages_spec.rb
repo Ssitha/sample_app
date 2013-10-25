45eeaf932e4c26ba82477f0f17b5d336f85506c9
sitha <sitha6@gmail.com>
2013-10-25 12:20:46 -0700

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -40,15 +40,26 @@ describe "AuthenticationPages" do
       describe "for non-signed-in users" do
         let(:user) { FactoryGirl.create(:user) }
 
-
-       describe "when attempting to visit a protected page" do
+        describe "when attempting to visit a protected page" do
           before do
             visit edit_user_path(user)
             fill_in "Email",    with: user.email
             fill_in "Password", with: user.password
             click_button "Sign in"
+          end 
+
+        describe "in the Relationships controller" do
+          describe "submitting to the create action" do
+            before { post relationships_path }
+            specify { expect(response).to redirect_to(signin_path) }
           end
 
+          describe "submitting to the destroy action" do
+            before { delete relationship_path(1) }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+        end
+        
           describe "in the Microposts controller" do
 
             describe "submitting to the create action" do
@@ -81,8 +92,18 @@ describe "AuthenticationPages" do
             before { patch user_path(user) }
             specify { expect(response).to redirect_to(signin_path) }
           end
+
+          describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
         end
 
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+      end
+    
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_title('Sign in') }
@@ -93,6 +114,7 @@ describe "AuthenticationPages" do
           let(:user) { FactoryGirl.create(:user) }
           let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
           before { sign_in user, no_capybara: true }
+        end
 
           describe "submitting a GET request to the Users#edit action" do
             before { get edit_user_path(wrong_user) }
