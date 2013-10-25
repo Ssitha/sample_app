d57ba533b7dc5fe9390f95a5b57db81f23c59914
sitha <sitha6@gmail.com>
2013-10-24 20:06:50 -0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def destroy
@@ -56,14 +57,7 @@ class UsersController < ApplicationController
 
     # Before filters
 
-    def signed_in_user
-     unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
-  end
-
-  def admin_user
+    def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe User do
+
 	before do
 		@user = User.new( name:'Example User', email:'user@example.com', password:'foobar', password_confirmation:'foobar')
 	end
@@ -18,6 +19,8 @@ describe User do
 	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
 	it { should respond_to(:admin) }
+	it { should respond_to(:microposts) }
+	it { should respond_to(:feed) }
 
 	describe "with admin attribute set to 'true'" do
 		before do
@@ -109,6 +112,31 @@ describe "with valid password" do
 	it { should == found_user.authenticate(@user.password) }
 end
 
+describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+  end
+end
+
+describe "status" do
+	let(:unfollowed_post) do
+		FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+	end
+
+	its(:feed) { should include(newer_micropost) }
+	its(:feed) { should include(older_micropost) }
+	its(:feed) { should_not include(unfollowed_post) }
+end
+
 describe "with invalid password" do
 	let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 	it { should_not == user_for_invalid_password }
@@ -119,5 +147,6 @@ end
 describe "remember token" do
 	before { @user.save }
 	its(:remember_token) { should_not be_blank }
+end
 end
 end
